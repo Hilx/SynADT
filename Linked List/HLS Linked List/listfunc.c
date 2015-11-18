0b9be6816200e68ce3a75c5e5eed644c4fa34412
Hilda <hilda1897@gmail.com>
2015-11-18 16:24:40 +0000

    correction
    next_t was defined as unsigned instead of signed before, huge mistake...

--- Linked List/HLS Linked List/listfunc.c
+++ Linked List/HLS Linked List/listfunc.c
@@ -1,55 +1,55 @@
 #include "top.h"
 
 /* Memory Node Access - Write Node */
-void node_write_next(data_t *Master2Mem, ptr_t nodePtr, next_t offset){
-	Master2Mem[nodePtr/4 + NEXT_OFFSET] = offset;
+void list_node_write_next(data_t *Master2Mem, ptr_t nodePtr, next_t offset){
+	Master2Mem[nodePtr/4 + LIST_NEXT_OFFSET] = offset;
 }
-void node_write_data(data_t *Master2Mem, ptr_t nodePtr, data_t data){
-	Master2Mem[nodePtr/4 + DATA_OFFSET] = data;
+void list_node_write_data(data_t *Master2Mem, ptr_t nodePtr, data_t data){
+	Master2Mem[nodePtr/4 + LIST_DATA_OFFSET] = data;
 }
-void node_write(data_t *Master2Mem, ptr_t nodePtr, struct node_t currentNode){
-	node_write_data(Master2Mem, nodePtr, currentNode.data);
-	node_write_next(Master2Mem, nodePtr, currentNode.next);
+void list_node_write(data_t *Master2Mem, ptr_t nodePtr, struct list_node_t currentNode){
+	list_node_write_data(Master2Mem, nodePtr, currentNode.data);
+	list_node_write_next(Master2Mem, nodePtr, currentNode.next);
 }
 
 /* Memory Node Access - Read Node */
-next_t node_read_next(data_t *Master2Mem, ptr_t nodePtr){
-	return Master2Mem[nodePtr/4 + NEXT_OFFSET];
+next_t list_node_read_next(data_t *Master2Mem, ptr_t nodePtr){
+	return Master2Mem[nodePtr/4 + LIST_NEXT_OFFSET];
 
 }
-data_t node_read_data(data_t *Master2Mem, ptr_t nodePtr){
-	return Master2Mem[nodePtr/4 + DATA_OFFSET];
+data_t list_node_read_data(data_t *Master2Mem, ptr_t nodePtr){
+	return Master2Mem[nodePtr/4 + LIST_DATA_OFFSET];
 }
-struct node_t node_read(data_t *Master2Mem, ptr_t nodePtr){
-	struct node_t readNode;
-	readNode.data = node_read_data(Master2Mem, nodePtr);
-	readNode.next = node_read_next(Master2Mem, nodePtr);
+struct list_node_t list_node_read(data_t *Master2Mem, ptr_t nodePtr){
+	struct list_node_t readNode;
+	readNode.data = list_node_read_data(Master2Mem, nodePtr);
+	readNode.next = list_node_read_next(Master2Mem, nodePtr);
 	return readNode;
 }
 
 /* Low-level list manipulation functions */
-void node_set_next(data_t *Master2Mem, ptr_t currentNodePtr, ptr_t nextNodePtr){
+void list_node_set_next(data_t *Master2Mem, ptr_t currentNodePtr, ptr_t nextNodePtr){
 	next_t offset;
 	offset = nextNodePtr - currentNodePtr;
-	node_write_next(Master2Mem, currentNodePtr, offset);
+	list_node_write_next(Master2Mem, currentNodePtr, offset);
 }
 
-ptr_t node_alloc_new(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t nextNodePtr){
+ptr_t list_node_alloc_new(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t nextNodePtr){
 	ptr_t newNodePtr;
-	struct node_t newNode;
-	newNodePtr = SysMalloc(sizeof(struct node_t), Master2SysAlloc);
+	struct list_node_t newNode;
+	newNodePtr = SysMalloc(sizeof(struct list_node_t), Master2SysAlloc);
 	newNode.data = data;
 	newNode.next = nextNodePtr - newNodePtr;
-	node_write(Master2Mem, newNodePtr, newNode);
+	list_node_write(Master2Mem, newNodePtr, newNode);
 	return newNodePtr;
 }
 
-ptr_t node_get_nextNodePtr(data_t *Master2Mem, ptr_t currentNodePtr){
-	next_t offset = node_read_next(Master2Mem, currentNodePtr);
+ptr_t list_node_get_nextNodePtr(data_t *Master2Mem, ptr_t currentNodePtr){
+	next_t offset = list_node_read_next(Master2Mem, currentNodePtr);
 	return currentNodePtr + offset;
 }
 
-void node_delete(data_t *Master2SysAlloc, ptr_t nodePtr){
+void list_node_delete(data_t *Master2SysAlloc, ptr_t nodePtr){
 	SysFree(nodePtr, Master2SysAlloc);
 }
 
@@ -63,7 +63,7 @@ ptr_t ListGen(data_t *Master2Mem, data_t *Master2SysAlloc, int SizeOfList, ptr_t
 	return hdPtr;
 }
 ptr_t Add2List(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t hdPtr){
-	hdPtr = node_alloc_new(Master2Mem, Master2SysAlloc, data, hdPtr);
+	hdPtr = list_node_alloc_new(Master2Mem, Master2SysAlloc, data, hdPtr);
 	return hdPtr;
 }
 /* Reverse the list */
@@ -75,12 +75,10 @@ ptr_t ReverseList(data_t *Master2Mem,ptr_t hdPtr){
 	while(hdPtr != NULL_PTR){
 		currentPtr = hdPtr;
 		
-		offset = node_read_next(Master2Mem, currentPtr);
+		offset = list_node_read_next(Master2Mem, currentPtr);
 		hdOldListPtr = currentPtr + offset;
-		
-		//offset = hdNewListPtr - currentPtr;
-		//node_write_next(Master2Mem, currentPtr, offset);
-		node_set_next(Master2Mem, currentPtr, hdNewListPtr);
+
+		list_node_set_next(Master2Mem, currentPtr, hdNewListPtr);
 		
 		hdNewListPtr = currentPtr;
 		
@@ -97,8 +95,8 @@ ptr_t DeleteList(data_t *Master2Mem, data_t *Master2SysAlloc, ptr_t hdPtr){
 	
 	while(hdPtr != NULL_PTR){
 		nowPtr = hdPtr;		
-		hdPtr = node_get_nextNodePtr(Master2Mem, nowPtr);
-		node_delete(Master2SysAlloc, nowPtr);
+		hdPtr = list_node_get_nextNodePtr(Master2Mem, nowPtr);
+		list_node_delete(Master2SysAlloc, nowPtr);
 	}
 	
 	return hdPtr;
--- Linked List/HLS Linked List/top.h
+++ Linked List/HLS Linked List/top.h
@@ -8,16 +8,24 @@
 
 typedef volatile int data_t;
 typedef volatile int ptr_t;
-typedef volatile unsigned next_t;
+typedef volatile signed next_t;
 
 #define NULL_PTR 0
-#define DATA_OFFSET 1
-#define NEXT_OFFSET 0
+#define LIST_DATA_OFFSET 1
+#define LIST_NEXT_OFFSET 0
 
-struct node_t{
+struct list_node_t{
 	data_t data;
 	next_t next;
 };
+/* counter stuff */
+//addresses
+#define COUNTER_BASE 0x43C20000 // <- change this according to your system
+#define COUNTER_RESULT 4 // only needed in ARM core if you want to read result back to software
+//commands
+#define C_START 1
+#define C_STOP 2
+#define C_RESET 3 // probably only needed in ARM core to reset the counter after the result has been read
 
 /* SysAlloc */
 #define MMU_BASE 0x43C00000
@@ -31,18 +39,18 @@ volatile void SysFree(int addr, data_t *Master2SysAlloc);
 
 /* List */
 /* Memory Node Access - Write Node */
-void node_write_next(data_t *Master2Mem, int nodePtr, unsigned offset);
-void node_write_data(data_t *Master2Mem, int nodePtr, data_t data);
-void node_write(data_t *Master2Mem, int nodePtr, struct node_t currentNode);
+void list_node_write_next(data_t *Master2Mem, int nodePtr, next_t offset);
+void list_node_write_data(data_t *Master2Mem, int nodePtr, data_t data);
+void list_node_write(data_t *Master2Mem, int nodePtr, struct list_node_t currentNode);
 /* Memory Node Access - Read Node */
-next_t node_read_next(data_t *Master2Mem, int nodePtr);
-data_t node_read_data(data_t *Master2Mem, int nodePtr);
-struct node_t node_read(data_t *Master2Mem, int nodePtr);
+next_t list_node_read_next(data_t *Master2Mem, int nodePtr);
+data_t list_node_read_data(data_t *Master2Mem, int nodePtr);
+struct list_node_t node_read(data_t *Master2Mem, int nodePtr);
 /* Low-level list manipulation functions */
-void node_set_next(data_t *Master2Mem, int currentNodePtr, int nextNodePtr);
-ptr_t node_alloc_new(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t nextNodePtr);
-ptr_t node_get_nextNodePtr(data_t *Master2Mem, ptr_t currentNodePtr);
-void node_delete(data_t *Master2SysAlloc, ptr_t nodePtr);
+void list_node_set_next(data_t *Master2Mem, int currentNodePtr, int nextNodePtr);
+ptr_t list_node_alloc_new(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t nextNodePtr);
+ptr_t list_node_get_nextNodePtr(data_t *Master2Mem, ptr_t currentNodePtr);
+void list_node_delete(data_t *Master2SysAlloc, ptr_t nodePtr);
 /* Software C level list manipulation functions */
 /* Creating a list */
 ptr_t ListGen(data_t *Master2Mem, data_t *Master2SysAlloc, int SizeOfList, ptr_t hdPtr);
