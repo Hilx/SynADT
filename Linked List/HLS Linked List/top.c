7abb6fe224837e932cae2c18204c5c9be5cf3973
Hilda <hilda1897@gmail.com>
2015-11-19 15:47:16 +0000

    timer dependency
    there was no dependency between list and timer before, so the scheduling
    wasn't sending the timer control requests at the correct time.

--- Linked List/HLS Linked List/top.c
+++ Linked List/HLS Linked List/top.c
@@ -8,14 +8,38 @@ int ListIP(data_t *SlaveAXI, data_t *Master2Mem, data_t *Master2SysAlloc){
 	#pragma HLS INTERFACE m_axi depth=1 port=Master2SysAlloc offset=off
 
 	ptr_t hdPtr = NULL_PTR;
-	int SizeOfList = 10;
+	ptr_t hdPtr_old;
+	int SizeOfList = 1310720;
 	
+	Master2SysAlloc[(COUNTER_BASE)/4] = C_START;
+
 	/* Creating a list */
+	Master2SysAlloc[(COUNTER_BASE)/4 + 1] = C_START;
+	hdPtr_old = hdPtr;
 	hdPtr = ListGen(Master2Mem, Master2SysAlloc, SizeOfList, hdPtr);
+	if(hdPtr != hdPtr_old){
+		Master2SysAlloc[(COUNTER_BASE)/4 + 1] = C_STOP;
+	}
+
+
 	/* Reverse the list */
+	Master2SysAlloc[(COUNTER_BASE)/4 + 2] = C_START;
+	hdPtr_old = hdPtr;
 	hdPtr = ReverseList(Master2Mem, hdPtr);
+	if(hdPtr != hdPtr_old){
+		Master2SysAlloc[(COUNTER_BASE)/4 + 2] = C_STOP;
+	}
+
 	/* Delete the list */
+	Master2SysAlloc[(COUNTER_BASE)/4 + 3] = C_START;
+	hdPtr_old = hdPtr;
 	hdPtr = DeleteList(Master2Mem, Master2SysAlloc, hdPtr);
+	if(hdPtr != hdPtr_old){
+		Master2SysAlloc[(COUNTER_BASE)/4 + 3] = C_STOP;
+	}
+
+
+	Master2SysAlloc[(COUNTER_BASE)/4] = C_STOP;
 
 	return hdPtr;
 }
