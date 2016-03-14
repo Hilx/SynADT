c13d1666ca9832d87e1c7d4f1253b70c9deea85f
Hilda <hilda1897@gmail.com>
2016-03-14 21:30:58 +0000

    fix (scheduling)

--- HashTable/HLS/hashtable SynADT HLS/SynADT_list_high_level.c
+++ HashTable/HLS/hashtable SynADT HLS/SynADT_list_high_level.c
@@ -1,41 +1,30 @@
 #include "SynADT.h"
 
 ptr_t Add2SortedList(data_t *Master2Mem, data_t *Master2SysAlloc, data_t data, ptr_t hdPtr){
-	// search insertion point
-	int flag_already_there = 0;
-	ptr_t insertPtr;
-	ptr_t keepPtr;
 	if(hdPtr == NULL_PTR){
-		insertPtr = NULL_PTR;
+		hdPtr = list_node_alloc_new(Master2Mem, Master2SysAlloc, data, NULL_PTR);
 	}else{
 		int flag_stop = 0;		
 		struct list_node_t_std nowNode;
 		ptr_t nowPtr = hdPtr;
-		while(flag_stop != 1 && nowPtr != NULL_PTR){
+		ptr_t prePtr = hdPtr;
+SEARCH: while(flag_stop != 1 && nowPtr != NULL_PTR){
 			nowNode = list_node_read_std(Master2Mem, nowPtr);
 			if(data > nowNode.data){
-				insertPtr = nowPtr;
-				flag_stop = 1;
+				flag_stop = 1;	
+				ptr_t newNodePtr = list_node_alloc_new(Master2Mem, Master2SysAlloc, data, nowPtr);
+				if(prePtr == hdPtr){
+					hdPtr = newNodePtr;
+				}else{
+					list_node_set_next_pointer(Master2Mem, prePtr, newNodePtr);
+				}		
 			}else if(data < nowNode.data){
-				// keep looking
-				keepPtr = nowPtr;
-				nowPtr = nowNode.nextPtr;
-				insertPtr = nowPtr;
+				prePtr = nowPtr;
+				nowPtr = nowNode.nextPtr;				
 				flag_stop = 0;
 			}else if(data == nowNode.data){
-				flag_already_there = 1;
 				flag_stop = 1;
 			}
-		}
-	}	
-	// insert node
-	ptr_t newNodePtr;
-	if(flag_already_there == 0){
-		newNodePtr = list_node_alloc_new(Master2Mem, Master2SysAlloc, data, insertPtr);
-		if(hdPtr == insertPtr){
-			hdPtr = newNodePtr;
-		}else{
-			list_node_set_next_pointer(Master2Mem, keepPtr, newNodePtr);
 		}
 	}
 	return hdPtr;
@@ -45,8 +34,7 @@ ptr_t SearchInList(data_t *Master2Mem, ptr_t hdPtr, int key){
 	ptr_t outPtr;
 	if(hdPtr == NULL_PTR){
 		outPtr = SEARCH_NOT_FOUND;
-	}else{
-		outPtr = SEARCH_NOT_FOUND;
+	}else{		
 		int flag_stop = 0;		
 		struct list_node_t_std nowNode;
 		ptr_t nowPtr = hdPtr;
@@ -57,7 +45,7 @@ ptr_t SearchInList(data_t *Master2Mem, ptr_t hdPtr, int key){
 				outPtr = nowPtr;
 			}else if(key > nowNode.data){
 				flag_stop = 1;
-				// the key doesn't exist!
+				outPtr = SEARCH_NOT_FOUND;
 			}else if(key < nowNode.data){
 				flag_stop = 0;
 				nowPtr = nowNode.nextPtr;
@@ -68,40 +56,29 @@ ptr_t SearchInList(data_t *Master2Mem, ptr_t hdPtr, int key){
 }
 
 ptr_t DeleteListNode_HT(data_t *Master2Mem, data_t *Master2SysAlloc, ptr_t hdPtr, int key){
-    // search
-	int flag_no_deletion = 1;
-	int deletionNodePtr;
 	struct list_node_t_std nowNode;
-	ptr_t keepPtr;
+	ptr_t prePtr, nowPtr;
 	if(hdPtr != NULL_PTR){
 		int flag_stop = 0;		
-		struct list_node_t_std nowNode;
-		ptr_t nowPtr = hdPtr;
+		nowPtr = hdPtr;
 		while(flag_stop != 1 && nowPtr != NULL_PTR){
-			nowNode = list_node_read_std(Master2Mem, nowPtr);
-			if(key == nowNode.data){
-				flag_stop = 1;
-				flag_no_deletion = 0;
-				deletionNodePtr = nowPtr;
+			nowNode = list_node_read_std(Master2Mem, nowPtr);			
+			if(key < nowNode.data){
+				flag_stop = 0;				
+				prePtr = nowPtr;
+				nowPtr = nowNode.nextPtr;
 			}else if(key > nowNode.data){
 				flag_stop = 1;
-				// the key doesn't exist!
-			}else if(key < nowNode.data){
-				flag_stop = 0;
-				keepPtr = nowPtr;
-				nowPtr = nowNode.nextPtr;
+			}else if(key == nowNode.data){
+				flag_stop = 1;				
+				if(nowPtr == hdPtr){
+					hdPtr = nowNode.nextPtr;
+					list_node_delete(Master2SysAlloc, nowPtr);
+				}else{
+					list_node_set_next_pointer(Master2Mem, prePtr, nowNode.nextPtr);
+					list_node_delete(Master2SysAlloc, nowPtr);					
+				}				
 			}
-		}
-	}	
-	// delete
-	ptr_t newNodePtr;
-	if(flag_no_deletion == 0){
-		if(hdPtr == deletionNodePtr){
-			hdPtr = nowNode.nextPtr;
-			list_node_delete(Master2SysAlloc, deletionNodePtr);
-		}else{
-			list_node_set_next_pointer(Master2Mem, keepPtr,nowNode.nextPtr);
-			list_node_delete(Master2SysAlloc, deletionNodePtr);
 		}
 	}
 	return hdPtr;		
--- HashTable/HLS/hashtable SynADT HLS/user_functions.c
+++ HashTable/HLS/hashtable SynADT HLS/user_functions.c
@@ -4,20 +4,18 @@ ptr_t PM_1_INSERTION(data_t *Master2Mem, data_t *Master2SysAlloc, int log2size){
 	// create table
 	ptr_t hdTable = CreateHashTable(Master2Mem, Master2SysAlloc, HASH_TABLE_SIZE);	
 	// insert keys
+	int i;
+	int mask_range = log2size - 1;
+	int iteration_range = 1 << mask_range;
+	int key;
 	if(TestCase == USE_SEQUENTIAL_KEYS){
-		int m = log2size - 1;
-		int iter = 1 << m;
-		int i;
-		for(i = 0; i < iter; i++){
-			int key = 16*(i+1);
+SEQ: for(i = 0; i < iteration_range; i++){
+			key = 16*(i+1);
 			InsertNodeHashTable(Master2Mem, Master2SysAlloc, hdTable, key);
 		}
 	}else{
-		int log2_RNG_num_range = log2size - 1; // generate half the range from RNG
-		int IterationRange = 1 << log2_RNG_num_range;
-		int i;
-		for(i = 0; i < IterationRange; i++){
-			int key = RandGen(log2_RNG_num_range);
+RAN: for(i = 0; i < iteration_range; i++){
+			key = RandGen(mask_range);
 			InsertNodeHashTable(Master2Mem, Master2SysAlloc, hdTable, key);
 		}
 	}
@@ -25,20 +23,20 @@ ptr_t PM_1_INSERTION(data_t *Master2Mem, data_t *Master2SysAlloc, int log2size){
 }
 
 ptr_t PM_2_CHECK_INSERTION(data_t *Master2Mem, data_t *Master2SysAlloc, ptr_t hdTable, int log2size){
+	int i;
+	int mask_range;
+	int iteration_range = 1 << (log2size - 1);
+	int key;
 	if(TestCase == USE_SEQUENTIAL_KEYS){
-		int m = log2size - 1;
-		int iter = 1 << m;
-		int i;
-		for(i = 0; i < iter; i++){
-			int key = RandGen(log2size+2);
+		mask_range = log2size + 2;
+SEQ: for(i = 0; i < iteration_range; i++){
+			key = RandGen(mask_range);
 			CheckInsert_HashTable(Master2Mem, Master2SysAlloc, hdTable, key);
 		}
 	}else{
-		int log2_RNG_num_range = log2size; // generate FULL range from RNG
-		int IterationRange = 1 << (log2_RNG_num_range - 1); // only get half of them
-		int i;
-		for(i = 0; i < IterationRange; i++){
-			int key = RandGen(log2_RNG_num_range);
+		mask_range = log2size;
+RAN: for(i = 0; i < iteration_range; i++){
+			key = RandGen(mask_range);
 			CheckInsert_HashTable(Master2Mem, Master2SysAlloc, hdTable, key);
 		}
 	}	
@@ -46,21 +44,18 @@ ptr_t PM_2_CHECK_INSERTION(data_t *Master2Mem, data_t *Master2SysAlloc, ptr_t hd
 }
 
 ptr_t PM_3_UPDATE(data_t *Master2Mem, data_t *Master2SysAlloc, ptr_t hdTable, int log2size){
+	int i, oldKey, newKey;
+	int iteration_range = 1 << (log2size - 1);
 	if(TestCase == USE_SEQUENTIAL_KEYS){
-		int m = log2size - 1;
-		int iter = 1 << m;
-		int i;
-		for(i = 0; i < iter; i++){
-			int oldKey = RandGen(log2size+2);
-			int newKey = RandGen(log2size+3);
+		for(i = 0; i < iteration_range; i++){
+			oldKey = RandGen(log2size+2);
+			newKey = RandGen(log2size+3);
 			UpdateNode_HashTable(Master2Mem, Master2SysAlloc, hdTable, oldKey, newKey);
 		}
 	}else{
-		int IterationRange = 1 << (log2size - 1);
-		int i;
-		for(i = 0; i < IterationRange; i++){
-			int oldKey = RandGen(log2size - 1);
-			int newKey = RandGen(log2size);
+		for(i = 0; i < iteration_range; i++){
+			oldKey = RandGen(log2size - 1);
+			newKey = RandGen(log2size);
 			UpdateNode_HashTable(Master2Mem, Master2SysAlloc, hdTable, oldKey, newKey);
 		}
 	}	
